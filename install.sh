de67ac20ceb9849dce6d982fec52d8b679c13408
Matthew Paiz <matt@fittech.io>
2017-11-13 14:47:46 -0800

    Updated Install Script

--- install.sh
+++ install.sh
@@ -3,4 +3,13 @@
 set -e
 set -u
 
-echo Hello World!!!
+PANDA_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+mkdir "Panda.Meow" || exit 1
+
+cd Panda.Meow
+
+git clone https://github.com/panda-meow/tail.git
+git clone https://github.com/panda-meow/tools.git
+git clone https://github.com/panda-meow/content.git
+git clone https://github.com/panda-meow/whiskers.git
